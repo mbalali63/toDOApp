0f6e2b2d884eca972e71f371b02825ea04d3a087
Mahdi Balali <mahdi.balali@gmail.com>
2024-03-06 06:24:36 +0330

    style implemented. As I have problem with selecting colors, no special styling was implemented. It is postponed for further trainings.

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import HeaderSec from "./components/HeaderSec"
 import SearchBar from "./components/SearchBar"
 import TasksList from "./components/TasksList"
 import Footer from "./components/Footer"
+import './../src/styles/app.css'
 
 function App() {
 
   return (
-    <>
+    <main>
       <HeaderSec />
       <SearchBar />
       <TasksList />
       <Footer />
 
-    </>
+    </main>
   )
 }
 
